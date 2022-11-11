dcbd52b9dada95b79aeb794863f684f510dd598d
alephao <7674479+alephao@users.noreply.github.com>
2022-11-11 00:47:10 -0300

    docs: add natspec

--- src/ERC721BatchTransfer.sol
+++ src/ERC721BatchTransfer.sol
@@ -6,6 +6,9 @@ import {IERC721} from "$/src/IERC721.sol";
 /// @title ERC721 Batch Transfer
 /// @author Aleph Retamal (https://github.com/alephao)
 /// @notice Transfer ERC721 tokens in batches to a single wallet or multiple wallets.
+/// @notice To use any of the methods in this contract the user has to approve this contract
+///         to control their tokens using either `setApproveForAll` or `approve` functions from
+//          the ERC721 contract.
 contract ERC721BatchTransfer {
     /// @dev 0x5f6f132c
     error InvalidArguments();
@@ -25,6 +28,11 @@ contract ERC721BatchTransfer {
         _;
     }
 
+    /// @notice Transfer multiple tokens to the same wallet using the ERC721.transferFrom method
+    /// @notice If you don't know what that means, use the `safeBatchTransferToSingleWallet` method instead
+    /// @param erc721Contract the address of the nft contract
+    /// @param to the address that will receive the nfts
+    /// @param tokenIds the list of tokens that will be transferred
     function batchTransferToSingleWallet(
         IERC721 erc721Contract,
         address to,
@@ -44,6 +52,10 @@ contract ERC721BatchTransfer {
         }
     }
 
+    /// @notice transfer multiple tokens to the same wallet using the `ERC721.safeTransferFrom` method
+    /// @param erc721Contract the address of the nft contract
+    /// @param to the address that will receive the nfts
+    /// @param tokenIds the list of tokens that will be transferred
     function safeBatchTransferToSingleWallet(
         IERC721 erc721Contract,
         address to,
@@ -63,12 +75,21 @@ contract ERC721BatchTransfer {
         }
     }
 
+    /// @notice Transfer multiple tokens to multiple wallets using the ERC721.transferFrom method
+    /// @notice If you don't know what that means, use the `safeBatchTransferToMultipleWallets` method instead
+    /// @notice The tokens in `tokenIds` will be transferred to the addresses in the same position in `tos`
+    /// @notice E.g.: if tos = [0x..1, 0x..2, 0x..3] and tokenIds = [1, 2, 3], then:
+    ///         0x..1 will receive token 1;
+    ///         0x..2 will receive token 2;
+    //          0x..3 will receive token 3;
+    /// @param erc721Contract the address of the nft contract
+    /// @param tos the list of addresses that will receive the nfts
+    /// @param tokenIds the list of tokens that will be transferred
     function batchTransferToMultipleWallets(
         IERC721 erc721Contract,
         address[] calldata tos,
         uint256[] calldata tokenIds
     ) external noZero {
-        if (msg.sender == address(0)) revert InvalidCaller();
         uint256 length = tokenIds.length;
         if (tos.length != length) revert InvalidArguments();
 
@@ -86,12 +107,20 @@ contract ERC721BatchTransfer {
         }
     }
 
+    /// @notice Transfer multiple tokens to multiple wallets using the ERC721.safeTransferFrom method
+    /// @notice The tokens in `tokenIds` will be transferred to the addresses in the same position in `tos`
+    /// @notice E.g.: if tos = [0x..1, 0x..2, 0x..3] and tokenIds = [1, 2, 3], then:
+    ///         0x..1 will receive token 1;
+    ///         0x..2 will receive token 2;
+    //          0x..3 will receive token 3;
+    /// @param erc721Contract the address of the nft contract
+    /// @param tos the list of addresses that will receive the nfts
+    /// @param tokenIds the list of tokens that will be transferred
     function safeBatchTransferToMultipleWallets(
         IERC721 erc721Contract,
         address[] calldata tos,
         uint256[] calldata tokenIds
     ) external noZero {
-        if (msg.sender == address(0)) revert InvalidCaller();
         uint256 length = tokenIds.length;
         if (tos.length != length) revert InvalidArguments();
 
